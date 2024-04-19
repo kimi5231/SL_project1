f0752752faabf1110a32ff643fda87c4bc035921
kimi5231 <djwjod@naver.com>
2024-04-19 22:23:31 +0900

    main에 있던 character_window를 Character 클래스에 넣음

--- source/character.py
+++ source/character.py
@@ -1,3 +1,22 @@
-class Character:
+from tkinter import *
+from cat import Cat
+
+
+class Character(Cat):
     def __init__(self):
         self.x, self.y = 0, 0
+        self.width, self.height = 50, 50
+        self.window = Tk()
+        self.init_self_window()
+
+    def init_self_window(self):
+        # 윈도우 크기와 위치 설정.
+        self.window.geometry(f'{self.width}x{self.height}+{self.x}+{self.y}')
+        # 윈도우 크기 조정 금지.
+        self.window.resizable(False, False)
+        # 윈도우 프레임 제거.
+        self.window.overrideredirect(1)
+        # 윈도우가 항상 가장 위에 있도록 설정
+        self.window.attributes("-topmost", True)
+        # 종료키 설정.
+        self.window.bind('<Escape>', lambda e: self.window.quit())
--- source/main.py
+++ source/main.py
@@ -1,19 +1,5 @@
-from tkinter import *
+from character import Character
 
 
-def set_character_window():
-    # 윈도우 크기와 위치 설정.
-    character_window.geometry('75x75+0+0')
-    # 윈도우 크기 조정 금지.
-    character_window.resizable(False, False)
-    # 윈도우 프레임 제거.
-    character_window.overrideredirect(1)
-    # 윈도우가 항상 가장 위에 있도록 설정
-    character_window.attributes("-topmost", True)
-
-
-character_window = Tk()
-set_character_window()
-# 종료키 설정.
-character_window.bind('<Escape>', lambda e: character_window.quit())
-character_window.mainloop()
+character_window = Character()
+character_window.window.mainloop()
