c5a193d649ce3c71912690e458540c5dc177c15e
kimi5231 <djwjod@naver.com>
2024-04-19 13:27:49 +0900

    캐릭터 윈도우 생성 및 기본 설정
    +main 파일 추가

---
+++ source/main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+
+def set_character_window():
+    # 윈도우 크기와 위치 설정.
+    character_window.geometry('75x75+0+0')
+    # 윈도우 크기 조정 금지.
+    character_window.resizable(False, False)
+    # 윈도우 프레임 제거.
+    character_window.overrideredirect(1)
+    # 윈도우가 항상 가장 위에 있도록 설정
+    character_window.attributes("-topmost", True)
+
+
+character_window = Tk()
+set_character_window()
+character_window.mainloop()
